0cb394fc7a826b468e7c2da181536a7dc2c9d563
kingrabbit0 <bohdanlutsenko760@gmail.com>
2024-06-14 14:03:34 -0400

    [feat] not fix issue
    The logo and mark image is not displayed even through the extension of image and path are correct.

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -2,8 +2,8 @@ const PageFooter = () => {
     return (
         <div className="justify-around flex items-center">
             <div className="w-2/3 justify-around flex my-[100px]">
-                <img className="" src="/assets/image/logo.png" alt="logo" />
-                <img src="/assets/image/mark.png" alt="mark" />
+                {/* <img className="" src="/assets/image/logo.png" alt="logo" /> */}
+                {/* <img src="/assets/image/mark.png" alt="mark" /> */}
             </div>
         </div>
     );
