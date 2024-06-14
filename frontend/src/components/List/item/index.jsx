a5e6c845f33c58bf46d654517141c3de9b21e828
kingrabbit0 <bohdanlutsenko760@gmail.com>
2024-06-14 13:44:07 -0400

    [feat] change image path in code

--- frontend/src/components/List/item/index.jsx
+++ frontend/src/components/List/item/index.jsx
@@ -7,11 +7,11 @@ const Item = () => {
                 </div>
                 <div className="absolute top-[40px] left-0 border-l-[3px] border-blue-300 h-[40px] justify-around flex items-center">
                     <p className="bg-green-200 text-lg w-[75px] text-center font-bold">$29.90</p>
-                    <p className="content-end text-base ml-[60px]">MORE</p><img className="ml-[10px]" src="/assets/image/1.png" alt="menu" />
+                    <p className="content-end text-base ml-[60px]">MORE</p><img className="ml-[10px]" src="/assets/image/more.png" alt="menu" />
                 </div>
             </div>
             <div className="my-[20px] mx-[15px] justify-around flex items-center">
-                <button><img className="" src="/assets/image/2.png" alt="Buy" /></button>
+                <button><img className="" src="/assets/image/download.png" alt="Buy" /></button>
                 <p className="text-white text-xl">BUY e-Itinerary</p>
             </div>
         </div>
